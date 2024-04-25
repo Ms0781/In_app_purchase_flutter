32169d388a01b58c0ea9583b7f4f4e4c656232a0
Mahammadsad Shaikh <mahammadsad@igexsolutions.in>
2024-04-25 17:47:19 +0530

    Temp Commit

--- lib/view/paywall_widget.dart
+++ lib/view/paywall_widget.dart
@@ -1,31 +1,34 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class OfferingWidget extends StatelessWidget {
   final String title;
   final String amount;
   final String description;
-  const OfferingWidget({super.key, required this.title, required this.amount, required this.description});
+
+  const OfferingWidget(
+      {super.key,
+      required this.title,
+      required this.amount,
+      required this.description});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: MediaQuery.of(context).size.width,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(10),
-        color: Colors.red
       ),
-      padding: const EdgeInsets.all(10),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
-          Row(children: [
-            Text(title,style: GoogleFonts.roboto(fontSize: 22,color: Colors.white),),
-            const SizedBox(width: 5,),
-            const Icon(Icons.lock,color: Colors.white,)
-          ],),
-          Text(description,style: GoogleFonts.roboto(fontSize: 18),)
+          Text(
+            title,
+            style: const TextStyle(fontWeight: FontWeight.w600, color: Colors.white),
+          ),
+          const SizedBox(height: 10,),
+          Text(description,
+            style: TextStyle(),
+          )
         ],
       ),
     );
